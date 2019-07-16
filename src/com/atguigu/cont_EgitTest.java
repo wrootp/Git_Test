fc78e3413d3aa6f3b0438de34c2462d8e61aabe1
wrootp <18391447833@163.com>
2019-07-16 19:23:19 +0800

    master更新

--- src/com/atguigu/cont_EgitTest.java
+++ src/com/atguigu/cont_EgitTest.java
@@ -3,6 +3,6 @@ package com.atguigu;
 public class cont_EgitTest {
 	public static void main(String[] args) {
 		System.out.println("AAAAA");
-		System.out.println("BBBBB");
+		System.out.println("DDDDD");
 	}
 }
